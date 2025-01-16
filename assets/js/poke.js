fd261d474fb80e821307c096bf1ef68abeec040a
juancasteell <151373606+juancasteell@users.noreply.github.com>
2025-01-16 16:11:54 +0100

    hacer funcion de tipos

--- assets/js/poke.js
+++ assets/js/poke.js
@@ -155,9 +155,11 @@ async function getPokemonRandomTipo(event) {
   // Obtiene la primera generación de Pokémon del objeto 'generations'
   let tipo = types[selectedType];
 
-  // Genera un ID aleatorio dentro de la primera generación
-  let randomId =
-    Math.floor(Math.random() * (tipo.end - tipo.start + 1)) + tipo.start;
+  //recuperar todos los pokemon del tipo seleccionado o recuperar todos los pokemon
+
+  //opcional--> vas mirando si el pokemon corresponde con el tipo seleccionado y sino lo hace, lo descartas
+
+  //random de todo los pokemon del tipo seleccionado (seria del array de pokemons)
 
   // Realiza una solicitud fetch a la API de Pokémon con el ID aleatorio generado
   let response = await fetch(`${BASE_URL}pokemon/${randomId}`);
